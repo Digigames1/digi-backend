fc82f8a29f430873275b19d7a436e7d0b4ff2aff
Digigames1 <brolexygrupp@gmail.com>
2025-04-22 15:22:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.DB_URL, // 🔁 тут замість MONGO_URL
+    dbName: "digi",
+    collectionName: "sessions"
+  }),
+  cookie: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 1000 * 60 * 60 * 2 // 2 години
+  }
+}));
+
 app.use(morgan("dev"));
 const session = require("express-session");
 
